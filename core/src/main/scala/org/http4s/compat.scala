a1f60272b1ae9ccb47c677413fc36e3c8d15fbdb
Ross A. Baker <ross@rossabaker.com>
2016-07-31 12:02:48 -0400

    Make compat a private object.

--- core/src/main/scala/org/http4s/compat.scala
+++ core/src/main/scala/org/http4s/compat.scala
@@ -2,16 +2,9 @@ package org.http4s
 
 /** Internal utilities to minimize differences between scalaz and cats versions
   * without affecting user experience. */
-package object compat 
-    extends scalaz.syntax.ToDataOps
+private[http4s] object compat
+  extends scalaz.syntax.ToDataOps
     with scalaz.syntax.ToTypeClassOps
     with scalaz.syntax.std.ToAllStdOps
     with scalaz.std.AllInstances
     with scalaz.DisjunctionFunctions
-{
-  def left[A](a: A): scalaz.-\/[A] =
-    scalaz.-\/(a)
-
-  def right[B](b: B): scalaz.\/-[B] =
-    scalaz.\/-(b)
-}
